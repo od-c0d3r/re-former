328a42aad367c06c3cbacd007be5956098397871
od-c0d3r <omar_rashad@aol.com>
2021-05-27 12:55:31 +0200

    Updating README and Fixing Workflow Linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new( username:"Test", 
-                      email:"test@test.com",
-                      password:'omaromar' )
+    @user = User.new(username: 'Test',
+                     email: 'test@test.com',
+                     password: 'omaromar')
   end
 
   def create
@@ -17,20 +17,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-      if @user.update(username: params[:user]["username"], password: params[:user]["password"], email: params[:user]["email"])
-        flash[:success] = "User was successfully updated"
-        redirect_to new_user_path
-      else
-        flash[:error] = "Something went wrong"
-        render 'edit'
-      end
+    if @user.update(username: params[:user]['username'], password: params[:user]['password'],
+                    email: params[:user]['email'])
+      flash[:success] = 'User was successfully updated'
+      redirect_to new_user_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
+    end
   end
-  
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
