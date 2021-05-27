086d11d2079e2bd2c4addc70168cd356d35ab6b2
od-c0d3r <omar_rashad@aol.com>
2021-05-27 12:37:23 +0200

    Editing User Backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+      if @user.update(username: params[:user]["username"], password: params[:user]["password"], email: params[:user]["email"])
+        flash[:success] = "User was successfully updated"
+        redirect_to new_user_path
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+
   private
   def user_params
-    params.require(:user).permit(:username, :email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
