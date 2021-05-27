91383b89468fc1e835e1923861572805962b6f70
od-c0d3r <omar_rashad@aol.com>
2021-05-27 11:06:35 +0200

    Railser Way (form_with) of creating Forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new( username:"Test", 
+                      email:"test@test.com",
+                      password:'omaromar' )
   end
 
   def create
-    @user = User.new(username:params[:username], email:params[:email],password:params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
